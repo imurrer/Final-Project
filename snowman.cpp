71d4703b001c9a084cd43442945f821523412a4e
Hannah Sarkey <38545089+hsarkey@users.noreply.github.com>
2018-12-06 20:37:11 -0500

    Update snowman.cpp

--- snowman.cpp
+++ snowman.cpp
@@ -12,7 +12,12 @@ Snowman::Snowman() {
   xc2 = 250, yc2 = 215;
   xc3 = 250, yc3 = 233;
   r1 =7, r2 = 8, r3 = 10;
- 
+}
+
+Snowman::~Snowman() 
+{}
+
+Snowman::display() {
   gfx_color(255, 255, 255);
   gfx_fill_circle(xc1, yc1, r1);
   gfx_fill_circle(xc2, yc2, r2);
@@ -22,6 +27,7 @@ Snowman::Snowman() {
   gfx_circle(xc1, yc1, r1);
   gfx_circle(xc2, yc2, r2);
   gfx_circle(xc3, yc3, r3);
-  
+ 
+}
   
   
