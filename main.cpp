77d267b32df053d846b33403045294239b4f595e
Hannah Sarkey <38545089+hsarkey@users.noreply.github.com>
2018-12-06 20:34:59 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,6 +3,18 @@
 #include "snowman.h"
 
 int main(){
+  bool loop = true;
+  char c;
+  
+  gfx_open(500, 700, "Frosty on the Run");
+  
+  while (loop) {
+    c = gfx_wait();
+    gfx_clear();
+    switch(c) {
+      case 'p' : // p to play game
+        break;
+      case '
   
   
   
